8a52f475a61060c199314ed546f923b5fd2b2399
naru770 <natsumushi770@gmail.com>
2025-10-13 21:43:56 +0900

    refactor: 不自然な箇所を修正

--- src/components/Snackbar/store.ts
+++ src/components/Snackbar/store.ts
@@ -11,14 +11,14 @@ type Actions = {
   close: () => void;
 };
 
-const DEFAULT_SNACKBAR_STATE: Omit<State, "isOpen"> = {
+const DEFAULT_SNACKBAR_STATE: State = {
+  isOpen: false,
   message: "",
   duration: 3000,
 };
 
 export const useStore = create<State & Actions>((set) => ({
   ...DEFAULT_SNACKBAR_STATE,
-  isOpen: false,
   open: (input) => set({ ...DEFAULT_SNACKBAR_STATE, ...input, isOpen: true }),
   close: () => set({ isOpen: false }),
 }));
